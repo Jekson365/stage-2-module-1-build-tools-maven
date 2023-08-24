8476c3b9d005169280a775b5c68c6064f410c657
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-24 17:12:56 +0400

    123

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package com.epam.utils;
 
 public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-        //code!
+    public static boolean isPositiveNumber(String str) {try {
+        double num = Double.parseDouble(str);
+        return num > 0;
+    } catch (NumberFormatException e) {
+        return false;
+    }
     }
 }
